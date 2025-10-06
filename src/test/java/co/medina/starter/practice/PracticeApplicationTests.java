19250794633b4031643e07cd4dd77de059d05225
Camilo Medina <medina1camilo@gmail.com>
2025-10-06 20:18:49 +0200

    [hotfix/from-release-to-snapshot] 2nd fix workflow

--- src/test/java/co/medina/starter/practice/PracticeApplicationTests.java
+++ src/test/java/co/medina/starter/practice/PracticeApplicationTests.java
@@ -1,13 +1,19 @@
 package co.medina.starter.practice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PracticeApplicationTests {
 
+    @InjectMocks
+    private PracticeApplication practiceApplication;
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(practiceApplication);
     }
 
 }
